63b2533f06b00f2b27c552d275d25393290085bc
Blanca Maria <flori_blanca@hotmail.com>
2014-12-17 23:03:32 +0100

    :tada:

---
+++ src/java/model/EmpleadoFacade.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package model;
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+/**
+ *
+ * @author Blanca
+ */
+@Stateless
+public class EmpleadoFacade extends AbstractFacade<Empleado> {
+    @PersistenceContext(unitName = "siacam_remoto_bxPU")
+    private EntityManager em;
+
+    @Override
+    protected EntityManager getEntityManager() {
+        return em;
+    }
+
+    public EmpleadoFacade() {
+        super(Empleado.class);
+    }
+    
+}
